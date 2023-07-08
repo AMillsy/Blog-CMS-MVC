59a3ca1feb472ac83b8cf079ccb2a9e742f74121
AMillsy <00adammills00@gmail.com>
2023-07-08 01:09:38 +0100

    able to login

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,51 @@
 const loginForm = document.querySelector(`#login`);
 const signUpForm = document.querySelector(`#sign-up`);
+const loginWarning = document.querySelector(`#login-warning`);
+const signupWarning = document.querySelector(`#signup-warning`);
 
-loginForm.addEventListener(`submit`, function (e) {
+loginForm.addEventListener(`submit`, async function (e) {
   e.preventDefault();
+  const formContainer = e.target.children[1];
+  const username = formContainer.children[1].value.trim();
+  const password = formContainer.children[3].value.trim();
+  if (!(username || password)) return;
+  try {
+    const response = await fetch(`/api/users/login`, {
+      method: `POST`,
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/`);
+    } else {
+      loginWarning.style.display = `inline`;
+      showWarning(loginWarning);
+    }
+  } catch (error) {
+    loginWarning.style.display = `inline`;
+    showWarning(loginWarning);
+  }
 });
 
-signUpForm.addEventListener(`submit`, function (e) {
+signUpForm.addEventListener(`submit`, async function (e) {
   e.preventDefault();
+  const formContainer = e.target.children[1];
+  const username = formContainer.children[1].value.trim();
+  const email = formContainer.children[3].value.trim();
+  const password = formContainer.children[5].value.trim();
+  console.log(username, email, password);
+  try {
+    const response = await fetch(`/api/users`, {
+      method: `POST`,
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {}
 });
+
+function showWarning(content) {
+  setTimeout(function () {
+    content.style.display = `none`;
+  }, 1000);
+}
