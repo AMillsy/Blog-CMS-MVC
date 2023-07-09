6b066520b67c2b553dfd3e902896c3d359baa5dd
AMillsy <00adammills00@gmail.com>
2023-07-10 00:08:45 +0100

    better seeding data for the deployment

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -10,34 +10,42 @@ const blogdata = [
   },
   {
     id: 2,
-    title: "blogtastic 2",
-    content: "something amazing is happening woop woop",
+    title:
+      "The Art of Creative Writing: Exploring the Boundless Realm of Imagination",
+    content:
+      "Creative writing is a captivating form of self-expression that allows individuals to delve into the boundless realm of imagination. Whether it's crafting compelling narratives, penning thought-provoking poetry, or inventing vivid worlds, creative writing offers an outlet for exploring emotions, ideas, and experiences. This article explores the art of creative writing, discussing various techniques such as character development, plot construction, and setting creation. It also highlights the importance of honing one's writing skills through practice and seeking inspiration from different sources. Ultimately, it celebrates the beauty of creative writing as a powerful means of communication and personal fulfillment.",
     user_id: 1,
   },
 
   {
     id: 3,
-    title: "blogtastic 3",
-    content: "something amazing is happening woop woop",
+    title:
+      "Unlocking the Secrets of Ancient Civilizations: Archaeological Discoveries that Rewrote History",
+    content:
+      "Archaeology is an awe-inspiring field that enables us to unlock the secrets of ancient civilizations and rewrite our understanding of history. This article showcases some remarkable archaeological discoveries that have shed light on the past. From unearthing lost cities and deciphering ancient scripts to unraveling cultural practices and technological advancements, these discoveries have expanded our knowledge of human civilization. The article delves into key findings such as the Rosetta Stone, the tomb of Tutankhamun, and the ruins of Machu Picchu, discussing their historical significance and the insights they offer into the lives of our ancestors.",
     user_id: 1,
   },
 
   {
     id: 4,
-    title: "blogtastic 4",
-    content: "something amazing is happening woop woop",
+    title:
+      "From Canvas to Digital: The Evolution of Visual Art in the Digital Age",
+    content:
+      "The digital age has brought about a remarkable evolution in the world of visual art, transforming traditional art practices and opening up new avenues for creativity. This article explores the impact of digital technology on visual art forms. It discusses how artists are embracing digital tools and techniques to create stunning digital paintings, 3D sculptures, and interactive installations. The article also delves into the rise of digital art communities and the opportunities offered by online platforms for sharing and promoting artwork. It celebrates the fusion of traditional and digital art, highlighting the vast possibilities that the digital age presents to artists.",
     user_id: 4,
   },
   {
     id: 5,
-    title: "blogtastic 5",
-    content: "something amazing is happening woop woop",
+    title: "The Science of Happiness: Exploring the Psychology of Well-being",
+    content:
+      " Happiness is a fundamental human aspiration, and the science of happiness seeks to understand the factors that contribute to our overall well-being. This article delves into the psychology of happiness, exploring various theories and research findings. It discusses the role of positive emotions, resilience, social connections, and purpose in cultivating happiness. The article also examines evidence-based practices such as gratitude exercises, mindfulness, and acts of kindness that can enhance well-being. By understanding the science behind happiness, we can make informed choices and adopt strategies to lead more fulfilling lives.",
     user_id: 5,
   },
   {
     id: 6,
-    title: "new BLOG",
-    content: "something amazing is happening woop woop",
+    title: "Beyond Earth: The Quest for Life in the Universe",
+    content:
+      "The search for life beyond Earth has fascinated scientists and sparked the imagination of humankind for centuries. This article delves into the ongoing quest to find extraterrestrial life and explores the scientific endeavors undertaken to unravel this profound mystery. It discusses the criteria for habitability in the universe, the exploration of Mars and other celestial bodies, and the discovery of potentially habitable exoplanets. The article also delves into the field of astrobiology, which examines the conditions necessary for life to exist elsewhere in the cosmos. While we have yet to find concrete evidence of extraterrestrial life, the search continues, driven by curiosity and the desire to understand our place in the universe.",
     user_id: 2,
   },
 ];
