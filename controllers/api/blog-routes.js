01f510d7be1f2c224653f06394889ed5aeb81ad8
AMillsy <00adammills00@gmail.com>
2023-07-09 13:37:24 +0100

    able to create new blogs

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -10,3 +10,30 @@ router.get(`/`, async (req, res) => {
 });
 
 module.exports = router;
+
+router.post(`/`, async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res
+      .status(400)
+      .json({ message: `Please fill out the title and the content` });
+  }
+
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.session.username,
+      },
+    });
+    const { id } = user.get({ plain: true });
+
+    const createdBlog = await Blog.create({
+      title: title,
+      content: content,
+      user_id: id,
+    });
+    res.status(200).json(createdBlog);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
--- public/js/account.js
+++ public/js/account.js
@@ -1,8 +1,34 @@
 const createBtn = document.querySelector(`#create-blog`);
+const warningText = document.querySelector(`.warning`);
 
-createBtn.addEventListener(`click`, function (e) {
+createBtn.addEventListener(`click`, async function (e) {
   const title = document.querySelector(`#title`).value;
   const content = document.querySelector(`#content`).value;
-  console.log(`hi`);
+
   console.log(title, content);
+
+  const resposne = await fetch(`/api/blogs`, {
+    method: `POST`,
+    body: JSON.stringify({ title, content }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (resposne.ok) {
+    location.reload();
+  } else {
+    const { message: responeData } = await resposne.json();
+    const warning =
+      responeData || `Sorry there has been an error uploading the blog`;
+    console.log(warning);
+    warningText.textContent = warning;
+    warningText.style.display = `inline`;
+    showWarning(warningText);
+  }
 });
+
+function showWarning(content) {
+  setTimeout(function () {
+    content.style.display = `none`;
+    content.textContent = ``;
+  }, 5000);
+}
